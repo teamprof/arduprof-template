937a68cc1edd1392e4e7324115f69701ffbcc0e7
teamprof.net <teamprof.net@gmail.com>
2026-04-25 14:38:37 +0800

    draft template

---
+++ src/pins.h
@@ -0,0 +1,31 @@
+/* Copyright 2026 teamprof.net@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#define PI_PICO // for Pi Pico/Pico2
+
+///////////////////////////////////////////////////////////////////////////////
+// PI_PICO
+#ifdef PI_PICO
+// #include "Arduino.h"
+
+// #define PIN_LED 25 // PICO_DEFAULT_LED_PIN: on-board LED (Green) note: already defined in pins_arduino.h, so no need to define it again here
+
+#endif // PI_PICO
